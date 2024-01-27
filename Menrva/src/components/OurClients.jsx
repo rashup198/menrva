7f8c7aed5290aad0cf03d14a90fdea8ecc91b964
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-01-27 11:57:01 +0530

    updated ourclients.jsx

--- Menrva/src/components/OurClients.jsx
+++ Menrva/src/components/OurClients.jsx
@@ -6,15 +6,15 @@ import img4 from '../assets/logo4.png';
 
 const OurClients = () => {
   return (
-    <div className="parent flex flex-col justify-center items-center mb-[30px]">
-      <h1 className=" text-[30px] font-semibold text-center md:mt-[30px] md:mb-[40px] underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]">
+    <div className="parent flex flex-col justify-center items-center mb-[30px] w-[95vw] mx-auto">
+      <h1 className=" text-[17px] font-semibold mt-[20px] mb-[20px] md:text-[30px] text-center md:mt-[30px] md:mb-[40px] underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]">
         Trusted By The India's Leading Companies
       </h1>
       <div className="childParent flex flex-row justify-center items-center flex-wrap gap-[20px] md:flex-row md:gap-[100px]">
-        <img src={img1} className="child w-[100px] md:w-[150px] lg:w-[200px]" alt="Hospital 1" />
-        <img src={img3} className="child w-[120px] md:w-[200px] lg:w-[200px]" alt="Hospital 3" />
-        <img src={img2} className="child w-[150px] md:w-[250px] lg:w-[300px]" alt="Hospital 2" />
-        <img src={img4} className="child w-[150px] md:w-[250px] lg:w-[200px]" alt="Hospital 4" />
+        <img src={img1} className="child w-[130px] md:w-[130px] " alt="Hospital 1" />
+        <img src={img3} className="child w-[120px] md:w-[200px] " alt="Hospital 3" />
+        <img src={img2} className="child w-[150px] md:w-[250px] " alt="Hospital 2" />
+        <img src={img4} className="child w-[150px] md:w-[250px] " alt="Hospital 4" />
       </div>
     </div>
   );
