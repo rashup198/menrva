5d84c7a42d05ad05de317710242773105390099a
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-01-27 11:57:24 +0530

    updated services.jsx

--- Menrva/src/components/Services.jsx
+++ Menrva/src/components/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { TiTick } from "react-icons/ti";
 import { FaCircleUser } from "react-icons/fa6";
 import { AiTwotoneMessage } from "react-icons/ai";
@@ -9,71 +9,75 @@ import { FaDollarSign } from "react-icons/fa";
 import { PiStudentBold } from "react-icons/pi";
 import { FaGlobe } from "react-icons/fa";
 import { FaRegCalendarCheck } from "react-icons/fa";
+
 const Services = () => {
-  const data =[
+  const data = [
     {
-        id:1,
-        logo:<TiTick />,
-        titel:"RECOMMENDATION SYSTEM",
-    },{
-        id:2,
-        logo:<FaCircleUser />,
-        titel:"CUSTOMER LIFECYCLE MANAGEMENT",
+      id: 1,
+      logo: <TiTick />,
+      title: "RECOMMENDATION SYSTEM",
     },
     {
-        id:3,
-        logo:<AiTwotoneMessage />,
-        titel:"NATURAL LANGUAGE PROCESSING",
+      id: 2,
+      logo: <FaCircleUser />,
+      title: "CUSTOMER LIFECYCLE MANAGEMENT",
     },
     {
-      id:4,
-      logo:<BsGraphUpArrow />,
-      titel:"FORECASTING"
-    },{
-      id:5,
-      logo:<FaTruckMoving />,
-      titel:"SUPPLY CHAIN ANALYTICS"
+      id: 3,
+      logo: <AiTwotoneMessage />,
+      title: "NATURAL LANGUAGE PROCESSING",
     },
     {
-      id:6,
-      logo:<FaRobot />,
-      titel:"GENERATIVE AI"
+      id: 4,
+      logo: <BsGraphUpArrow />,
+      title: "FORECASTING"
+    },
+    {
+      id: 5,
+      logo: <FaTruckMoving />,
+      title: "SUPPLY CHAIN ANALYTICS"
+    },
+    {
+      id: 6,
+      logo: <FaRobot />,
+      title: "GENERATIVE AI"
     }
-]
+  ];
 
-const data1 =[
-  {
-      id:1,
-      logo:<FaDollarSign />,
-      titel:"$12+ MN",
-      desc:"Business Impact"
+  const data1 = [
+    {
+      id: 1,
+      logo: <FaDollarSign />,
+      title: "$12+ MN",
+      desc: "Business Impact"
+    },
+    {
+      id: 2,
+      logo: <PiStudentBold />,
+      title: "20+",
+      desc: "Domain Experts"
+    },
+    {
+      id: 3,
+      logo: <FaGlobe />,
+      title: "10+",
+      desc: "Global Clients"
+    },
+    {
+      id: 4,
+      logo: <FaRegCalendarCheck />,
+      title: "30+",
+      desc: "Projects Completed"
+    }
+  ];
 
-  },
-  {
-    id:2,
-    logo:<PiStudentBold />,
-    titel:"20+",
-    desc:"Domain Experts"
-  },{
-    id:3,
-    logo:<FaGlobe />,
-    titel:"10+",
-    desc:"Global Clients"
-  },
-  {
-    id:4,
-    logo:<FaRegCalendarCheck />,
-    titel:"30+",
-    desc:"Projects Completed"
-  }
-]
-return (
-<div>
-  <div className="parrent flex flex-col gap-[50px] bg-[#eaeffa]">
-    <h1 className=' text-[30px] font-semibold text-center mt-[30px] mb-[30px] underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]'>
-    OUR EXPERTISE
-    </h1>
-    
+  return (
+    <div>
+      <div className="parrent flex flex-col gap-10 md:gap-16 bg-[#eaeffa]">
+        <h1 className='text-3xl md:text-4xl font-semibold text-center mt-5 mb-5 md:mt-10 md:mb-10 underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]'>
+          OUR EXPERTISE
+        </h1>
+
         <div className="flex flex-row flex-wrap justify-evenly items-center w-[80vw] mx-auto gap-[80px] ">
             {
                 data.map((item)=>(
@@ -81,30 +85,28 @@ return (
                         <div className="logo bg-[#5e17EB] rounded-full p-3">
                            <p className=' text-4xl font-extrabold text-white'>{item.logo}</p> 
                         </div>
-                        <h1 className=' text-[20px]'>{item.titel}</h1>
+                        <h1 className=' text-[10px] md:text-[20px]'>{item.title}</h1>
                     </div>
                 ))
             }
         </div>
 
-          <div className="bg-[#5e17EB] pt-[30px] pb-[30px]">
-            <div className=" flex flex-wrap w-[80vw] justify-evenly  mx-auto">
-            {
-              data1.map((item)=>(
-                <div className="flex flex-col justify-center items-center gap-[15px]">
-                  <div className="logo bg-white rounded-md p-5">
-                     <p className=' text-5xl font-extrabold text-[#5e17EB]'>{item.logo}</p> 
-                  </div>
-                  <h1 className=' text-[30px] font-bold text-white'>{item.titel}</h1>
-                  <p className=' text-[20px] text-white'>{item.desc}</p>
+        <div className="bg-[#5e17EB] pt-10 pb-10">
+          <div className="flex flex-wrap w-[80vw] justify-evenly mx-auto">
+            {data1.map((item) => (
+              <div key={item.id} className="flex flex-col justify-center items-center gap-4 md:gap-2">
+                <div className="logo bg-white rounded-md p-5">
+                  <p className='text-5xl font-extrabold text-[#5e17EB]'>{item.logo}</p>
+                </div>
+                <h1 className='text-3xl font-bold text-white'>{item.title}</h1>
+                <p className='text-xl text-white mb-[20px]'>{item.desc}</p>
               </div>
-              ))
-            }  
-            </div>
-          </div>    
-  </div>
-</div>
-)
-}
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
