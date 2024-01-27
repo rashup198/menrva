8220c2259eeb32ac81fff326d11c4f9f9943acb0
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-01-27 11:58:01 +0530

    updated values.jsx

--- Menrva/src/components/Values.jsx
+++ Menrva/src/components/Values.jsx
@@ -1,64 +1,61 @@
-import React from 'react'
-import { FaUserCheck } from "react-icons/fa";
-import { FaHandshake } from "react-icons/fa";
-import { FaLightbulb } from "react-icons/fa";
-import { FaStar } from "react-icons/fa6";
-import { FaTrophy } from "react-icons/fa";
-import { FaUsers } from "react-icons/fa";
+import React from 'react';
+import { FaUserCheck, FaHandshake, FaLightbulb, FaStar, FaTrophy, FaUsers } from "react-icons/fa";
+
 const Values = () => {
-    const data =[
-        {
-            id:1,
-            logo:<FaUserCheck />,
-            titel:"CUSTOMER FIRST"
-        },
-        {
-            id:2,
-            logo:<FaHandshake />,
-            titel:"INTEGRITY"
-        },
-        {
-            id:3,
-            logo:<FaLightbulb />,
-            titel:"INNOVATION"
-        }
-        ,{
-            id:4,
-            logo:<FaStar />,
-            titel:"EXCELLENCE"
-        },{
-            id:5,
-            logo:<FaTrophy />,
-            titel:"RESULTS DRIVEN"
-        },{
-            id:6,
-            logo:<FaUsers />,
-            titel:"DIVERSITY & INCLUSION"
-        }
-    ]
+  const data = [
+    {
+      id: 1,
+      logo: <FaUserCheck />,
+      title: "CUSTOMER FIRST"
+    },
+    {
+      id: 2,
+      logo: <FaHandshake />,
+      title: "INTEGRITY"
+    },
+    {
+      id: 3,
+      logo: <FaLightbulb />,
+      title: "INNOVATION"
+    },
+    {
+      id: 4,
+      logo: <FaStar />,
+      title: "EXCELLENCE"
+    },
+    {
+      id: 5,
+      logo: <FaTrophy />,
+      title: "RESULTS DRIVEN"
+    },
+    {
+      id: 6,
+      logo: <FaUsers />,
+      title: "DIVERSITY & INCLUSION"
+    }
+  ];
+
   return (
     <div>
-      <div className="parrent flex flex-col gap-[30px] bg-[#eaeffa] justify-center items-center h-[400px]">
+      <div className="parent flex flex-col gap-8 md:gap-12 bg-[#eaeffa] justify-center items-center py-16">
 
-    <h1 className=' text-[30px] font-semibold text-center mb-[30px] underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]'>
-    OUR VALUES
-    </h1>
-    
-        <div className="flex flex-row flex-wrap justify-evenly items-center w-[60vw] gap-x-[100px] gap-y-[40px]">
-            {
-                data.map((item)=>(
-                    <div className="child flex flex-col justify-evenly items-center gap-[15px]">
-                        <div className="logo bg-[#5e17EB] rounded-full p-3">
-                           <p className=' text-4xl font-extrabold text-white'>{item.logo}</p> 
-                        </div>
-                        <h1 className=' text-[20px]'>{item.titel}</h1>
-                    </div>
-                ))
-            }
+        <h1 className='text-3xl md:text-4xl font-semibold text-center mb-8 md:mb-12 underline underline-offset-8 text-[#5e17EB] decoration-[#5e17EB]'>
+          OUR VALUES
+        </h1>
+
+        <div className="flex flex-wrap justify-center items-center gap-8 md:w-[70vw] mx-auto md:gap-16">
+          {data.map((item) => (
+            <div key={item.id} className="child flex flex-col justify-center items-center gap-4">
+              <div className="logo bg-[#5e17EB] rounded-full p-4">
+                <p className='text-2xl md:text-4xl font-extrabold text-white'>{item.logo}</p>
+              </div>
+              <h1 className='text-xl md:text-2xl'>{item.title}</h1>
+            </div>
+          ))}
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Values
+export default Values;
